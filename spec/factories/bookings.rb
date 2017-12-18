2f27f55fa097fe5c112547fc1bbf2bc8e9739b4a
Adam <ag.gradowski@gmail.com>
2017-12-18 19:46:57 +0100

    Add models specs
    Add traits to Rental and Booking factories. Add model specs

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -5,5 +5,26 @@ FactoryBot.define do
     sequence(:client_email) { |n| "tester#{n}@mail.com"}
     price 100
     association :rental
+
+    trait :invalid_start_at do
+      start_at nil
+    end
+
+    trait :invalid_end_at do
+      end_at nil
+    end
+
+    trait :invalid_price do
+      price nil
+    end
+
+    trait :invalid_client_email do
+      client_email nil
+    end
+
+    trait :ends_before_start_at do
+      start_at Date.current.in_time_zone
+      end_at 1.days.ago
+    end
   end
 end
--- spec/factories/rentals.rb
+++ spec/factories/rentals.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
   factory :rental do
     sequence(:name) { |n| "Test Rental #{n}"}
     daily_rate 15
+
+    trait :invalid_name do
+      name nil
+    end
+
+    trait :invalid_daily_rate do
+      daily_rate nil
+    end
   end
 end
